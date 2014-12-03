ee0220181d7eeb2f50e47dd82520d4eed316e88b
Cappy Popp <c.popp@yahoo.com>
2014-12-03 18:52:04 -0500

    added setUp and tearDown - but may refactor to use mocks

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 __author__ = 'cappy'
 
 import unittest
+
+import MySQLdb as mdb
+
+from mock import Mock
 from tlinsights.constants import DB
-from tlinsights import db
+
+
+class TestDB(unittest.TestCase):
+    def setUp(self):
+        try:
+
+            cxn = mdb.connect(DB.DB_SERVER, DB.DB_USER, DB.DB_PASS, DB.DB_NAME, use_unicode=True, charset=DB.DB_CHARSET)
+
+        except mdb.MySQLError, e:
+            print "Error: {:d}{}".format(e.args[0], e.args[1])
+            exit(e.args[0])
+
+
+def tearDown(self):
+    pass
+
 
 if __name__ == '__main__':
     unittest.main()
