5e3854e40ba9352c9893b03d814696e484b4aa28
Cappy Popp <c.popp@yahoo.com>
2015-02-09 18:46:21 -0500

    added new TLGoSDictUnicodeReader class to handle reading R6 imports

--- csvdict/csvdict.py
+++ csvdict/csvdict.py
@@ -63,8 +63,31 @@ class DictUnicodeReader(object):
     def next(self):
         row = self.reader.next()
         #return [unicode(s, "utf-8") for s in row]
-        outrow = dict([(k, unicode(v, "utf-8")) for k,v in row.iteritems()])
+        outrow = dict([(k, unicode(v, "utf-8")) for k, v in row.iteritems()])
         return outrow
 
     def __iter__(self):
         return self
+
+class TLGoSDictUnicodeReader(DictUnicodeReader):
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        super(TLGoSDictUnicodeReader, self).__init__(self, f, dialect, encoding, **kwds)
+
+    def load_tweets_from_csv(self, authors=None):
+
+        tweets = {"from_author": [], "not_from_author": []}
+
+        if authors is None:
+            authors = []  # have to do this this way because can't have default mutable args in Python
+        for index, row in enumerate(self):
+            # skip the header row and any retweets since we don't care about them,
+            # also filter by author == AVGFree
+            if index != 0:  # and 'RT @' not in row['CONTENT']:  # skips header
+                tweet_id = int(row['ARTICLE_URL'].rsplit("/", 1)[1])
+                if row["AUTHOR"] in authors:
+                    tweets["from_author"].append(tweet_id)
+                else:
+                    tweets["not_from_author"].append(tweet_id)
+
+        return tweets
